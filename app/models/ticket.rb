16bc69722ac0af82befb2a65a4cabe3e5b67a4d4
Maximiliano Garcia Roe <mrgarcia1@miuandes.cl>
2018-04-09 16:11:44 -0300

    Copy Repo Lab04

---
+++ app/models/ticket.rb
@@ -0,0 +1,6 @@
+class Ticket < ApplicationRecord
+  belongs_to :event
+  has_many :user_tickets
+  has_many :users, through: :user_tickets
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  	has_many :user_tickets	
+	has_many :tickets, :through => :user_tickets
+end
